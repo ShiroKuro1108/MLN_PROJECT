75f3318ddfaeafc92acf422a605a93b24e4c4a40
ShiroKuro1108 <137161019+ShiroKuro1108@users.noreply.github.com>
2025-11-04 23:22:49 +0700

    update main scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;  // Để dùng Button
+
+public class MainMenu : MonoBehaviour
+{
+    public Button startButton;  // Assign StartButton trong Inspector
+
+    void Start()
+    {
+        if (startButton)
+        {
+            startButton.onClick.AddListener(StartGame);
+        }
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");  // Tên scene game chính
+    }
+}
